98c06b0427eb7393fef6cc83403e5f5f703d6fea
devArm <67116310+ArmLE@users.noreply.github.com>
2020-10-31 10:35:22 -0500

    Layout BCP-Banca Online Session

---
+++ README.txt
@@ -0,0 +1,2 @@
+Author: Lopez Espinoza, Armando Jorge
+Description: A version of session bcp - Perú.
---
+++ js/mainScript.js
@@ -0,0 +1,26 @@
+function nameOfDay(dayOfWeek){
+    Date.now();
+    var names = ["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes"]
+    return names[dayOfWeek];
+}
+function toArequipaDay(date1, date2){
+
+    return days;
+}
+function split(String word){
+    var result="";
+    for(i=0:word.lenght():i++){
+        word.charAt
+        result+=word.indexO
+    }
+}
+
+function ej1(Date date){
+    switch (date.getDay()){
+        case 1: 
+        case 2: "jueves"
+        case 3: "jueves"
+        case 4: "jueves"
+        case 5: "jueves"
+    }
+}
